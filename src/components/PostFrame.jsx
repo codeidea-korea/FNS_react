d5ceb1da5f852f02287d956193cd58cf3b770e86
parkbumjin1 <parkbumjin719@gmail.com>
2024-07-10 16:20:18 +0900

    fix 공통 모달 이벤트 함수

--- src/components/PostFrame.jsx
+++ src/components/PostFrame.jsx
@@ -19,6 +19,8 @@ const PostFrame = ({data,descOpen})=>{
     const desc = data.desc.split('<br/>');
     const [swiperActive, setSwiperActive] = useState(0)
     const navigate = useNavigate();
+    // 모달 Open/Close 이벤트
+    const { modalId, openModal, closeModal, clickModalEvent, renderAppDownloadModal } = Modal();
 
     const descHandle = (e)=>{
         let descBox = e.currentTarget.querySelector('.desc');
@@ -35,8 +37,6 @@ const PostFrame = ({data,descOpen})=>{
             navigate('/posts')
         }
     }
-
-    const { modalId, openModal, closeModal, clickModalEvent } = Modal();
 
     return (
         <section className="post_frame">
@@ -159,26 +159,7 @@ const PostFrame = ({data,descOpen})=>{
                 </div>
             )}
 
-            {/* 좋아요,댓글,공유 버튼 인터렙션 모달 */}
-            {modalId === 'appDownload' && (
-                <div className="modal_wrap open">
-                    <div className="modal_bg" onClick={closeModal}></div>
-                    <div className="modal_box">
-                        <div className="modal_content appdown_cont">
-                            <img src="/img/logo.svg" alt="패션&스타일 로고"/>
-                            <h6>패션&스타일 앱에서 제공되는 혜택을<br/>놓치고 계신 건 아닌가요?</h6>
-                            <p>패션&스타일 앱은 다양한 혜택 및 코디 탭을 제공 중!</p>
-                            <button className='down_btn'>
-                                <span>앱 이용하기</span>
-                            </button>
-                            <button className='close_btn' onClick={closeModal}>
-                                <span>괜찮아요, 모바일웹으로 볼게요.</span>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            )}
-
+            {renderAppDownloadModal()}
         </section>
     )
 }
--- src/page/common/Modal.jsx
+++ src/page/common/Modal.jsx
@@ -26,7 +26,33 @@ const Modal = () => {
         }
     }
 
-    return { modalId, openModal, closeModal, clickModalEvent };
+    /* 좋아요,댓글,공유 버튼 인터렙션 모달 */
+    const renderAppDownloadModal  = () => {
+        if(modalId === 'appDownload') {
+            return (
+                <div className="modal_wrap open">
+                    <div className="modal_bg" onClick={closeModal}></div>
+                    <div className="modal_box">
+                        <div className="modal_content appdown_cont">
+                            <img src="/img/logo.svg" alt="패션&스타일 로고"/>
+                            <h6>패션&스타일 앱에서 제공되는 혜택을<br/>놓치고 계신 건 아닌가요?</h6>
+                            <p>패션&스타일 앱은 다양한 혜택 및 코디 탭을 제공 중!</p>
+                            <button className='down_btn'>
+                                <span>앱 이용하기</span>
+                            </button>
+                            <button className='close_btn' onClick={closeModal}>
+                                <span>괜찮아요, 모바일웹으로 볼게요.</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+
+        return null;
+    }
+
+    return {modalId, openModal, closeModal, clickModalEvent, renderAppDownloadModal};
 }
 
 export default Modal;
